e4beb05d3f05e167cd4358b219bde291c13f5274
reilo <rl@lorein.de>
2016-03-19 08:55:10 +0100

    Update.

--- model/tournament.js
+++ model/tournament.js
@@ -2,23 +2,34 @@ var mongoose = require('mongoose');
 var utils = require('../utils/utils');
 
 const TournamentSchema = new mongoose.Schema({
-    type: {type: String, default:'league', enum:['league', 'challenge']},
-    id: {type: String, default: ''},
-    description: {type: String, default: ''},
-    organization: {type: String, default: ''},
-    status: {type: String, default: 'planned', enum: ['planned', 'progress', 'completed']},
+    type: {
+        type: String,
+        default: 'league',
+        enum: ['league', 'challenge']
+    },
+    id: {
+        type: String,
+        default: ''
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    organization: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'planned',
+        enum: ['planned', 'progress', 'completed']
+    },
     participants: [String]
 });
 
-TournamentSchema
-    .path('description').required(true, 'Description must be set.')
-
-    .path('organization').required(true, 'Organization must be set.')
-
-    .pre('save', function (next) {
-        this.id = utils.guid();
-        next();
-    })
-;
+TournamentSchema.pre('save', function (next) {
+    this.id = utils.guid();
+    next();
+});
 
 mongoose.model('Tournament', TournamentSchema);
