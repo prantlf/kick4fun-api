bc3907fccc67e0b0b9a091e3d3647f7dcb28e115
reilo <rl@lorein.de>
2016-03-25 10:46:06 +0100

    Update.

--- routes/organizers.js
+++ routes/organizers.js
@@ -36,14 +36,34 @@ router.post('/api/organizers', function (request, response, next) {
 router.put('/api/organizers/:id', function (request, response, next) {
     var id = request.params.id;
     var organizer = request.body;
-    Organizer.update({_id: id}, organizer/*, {runValidators: true}*/, function(error) {
-    //Organizer.findOneAndUpdate({'_id': id}, organizer, {upsert: true}, function (error) {
+    Organizer.findById(id, function(err, updatedOrganizer){
+        updatedOrganizer.longName = organizer.longName;
+        updatedOrganizer.save(function (error, organizer) {
+            if (error) {
+                next(new Error(error));
+            } else {
+                response.send(organizer);
+            }
+        });
+    });
+    /*
+    var updatedOrganizer = new Organizer({
+        _id: organizer.name,
+        longName: organizer.longName,
+        description: organizer.description || '',
+        adminUser: organizer.adminUser || '',
+        adminPassword: organizer.adminPassword || ''
+    });
+    updatedOrganizer.save(function (error, organizer) {
+    */
+    //Organizer.update({_id: id}, organizer/*, {runValidators: true}*/, function(error) {
+    /*Organizer.findOneAndUpdate({'_id': id}, organizer, {upsert: true}, function (error) {
         if (error) {
             next(new Error(error));
         } else {
             response.send(organizer);
         }
-    });
+    });*/
 });
 
 router.delete('/api/organizers/:id', function (request, response, next) {
