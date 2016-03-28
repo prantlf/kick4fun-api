567a44ba4e1f32bedcbc1b8543a86e314f02ee68
reilo <rl@lorein.de>
2016-03-28 10:38:30 +0200

    Update.

--- routes/participants.js
+++ routes/participants.js
@@ -36,15 +36,67 @@ router.post('api/organizers/:id/tournaments/:name/participants', function (reque
         if (error) {
             next(error);
         } else if (tournament == null) {
-            next('Tournament does not exist for given Organizer');
+            next(new Error('Tournament does not exist for given Organizer'));
+        } else if (tournament.kind == 'League' && tournament.status != 'build') {
+            next(new Error('Tournament/League is not in build status'));
+        } else if (tournament.kind == 'Challenge' && (tournament.status != build || tournament.status != 'progress')){
+            next(new Error('Tournament/Challenge is not in build or progress status'));
         } else if (tournament.participants.indexOf(participantName) >= 0) {
-            next('Player is already participant of tournament')
+            next(new Error('Player is already participant of tournament'));
         } else {
             tournament.participants.push(participantName);
         }
+        if (tournament.kind == 'Challenge') {
+            tournament.lineUp.push({
+                player: participantName,
+                score: options.score || 0,
+                fineScore: options.fineScore || 0
+            });
+        }
+        tournament.save(function (error, tournament) {
+            if (error) {
+                next(new Error(error));
+            } else {
+                response.send(tournament);
+            }
+        });
     });
 });
 
-//router.delete('api/organizers/:id/tournaments/:tname/participants/:pname')
+router.delete('api/organizers/:id/tournaments/:tname/participants/:pname', function (request, response, next) {
+    var participantName = request.body.pname;
+    var organizerId = request.params.id;
+    var tournamentName = request.params.tname;
+    Tournament.findOne({_organizer: organizerId, name: tournamentName}).exec(function (error, tournament) {
+        if (error) {
+            next(error);
+        } else if (tournament == null) {
+            next(new Error('Tournament does not exist for given Organizer'));
+        } else if (tournament.status != 'build') {
+            next(new Error('Tournament is not in build status'));
+        } else if (tournament.participants.indexOf(participantName) < 0) {
+            next(new Error('Player is not participant of tournament'));
+        } else {
+            tournament.participants.remove(participantName);
+        }
+        if (tournament.kind = 'Challenge') {
+            for (var i = 0; i < tournament.lineUp.length; ++i) {
+                if (tournament.lineUp[i].player == participantName) {
+                    tournament.lineUp.splice(i);
+                    break;
+                }
+            }
+        }
+        tournament.save(function (error, tournament) {
+            if (error) {
+                next(new Error(error));
+            } else {
+                response.send(tournament);
+            }
+        });
+    });
+});
+
+
 
 module.exports = router;
